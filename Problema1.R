b0abed712f9a49f6831b12b031951f97ebfe3b5b
Janserranoxufre <jan.serrano@estudiantat.upc.edu>
2025-03-04 09:46:16 +0100

    Update Problema1.R

--- Problema1.R
+++ Problema1.R
@@ -1,1 +1,15 @@
 #Codigo para problema 1
+Rsq=0.1923 #Mirant l'enunciat, Rsq= Coeficient dde determinació o bondat d'ajust
+R=sqrt(Rsq)
+R
+
+#Variació per unitat de mercat = pendent de la gràfica (variació de y per variació d'unitat de x)
+m <- 0.06576
+m
+
+b <- 11.79755 #intercept de la foto enunciat
+
+pred <- m*647.1+b
+#pred=54.35085
+
+#gràfic b, al voltant de 647.1 la y dona 54.35 mirar quina de les 3 s'assembla més
